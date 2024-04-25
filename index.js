7d857c2f1a56a6ad217eca0de197255beec88b91
samiceto <you@example.com>
2024-04-26 00:43:50 +0500

    this game is ready to use

---
+++ index.js
@@ -0,0 +1,13 @@
+import inquirer from "inquirer";
+let randomNumber = Math.floor(Math.random() * 10 + 1);
+let answer = await inquirer.prompt([{
+        name: "comingnumber",
+        type: "number",
+        message: "please inter a number between 1 to 10"
+    }]);
+if (randomNumber === answer.comingnumber) {
+    console.log("your number has matched the figer congrated");
+}
+else {
+    console.log("your number did'nt macthed please try again");
+}
